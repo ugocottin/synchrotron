0477cfdac7f0fca0b15050a38430109ef1121286
ugocottin <ugo.cottin@gmail.com>
2022-02-11 09:54:15 +0100

    fs interface

---
+++ src/fr/synchrotron/fs/FileSystem.java
@@ -0,0 +1,28 @@
+package fr.synchrotron.fs;
+
+import java.io.File;
+import java.util.List;
+
+public interface FileSystem {
+
+	public String getRoot();
+
+	public String getParent(String path);
+
+	public List<String> getChildren(String path);
+
+	public List<String> getAncestors(String path);
+
+	public String getAbsolutePath(String relativePath);
+
+	public String getRelativePath(String absolutePath);
+
+	public void replace(String absolutePathTargetFS, FileSystem fsSource, String absolutePathSourceFS);
+
+	public FileSystem getReference();
+
+	public File createDirectory(String path);
+
+	public void fileCopy(File input, File output) throws Exception;
+
+}
